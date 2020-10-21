0b26bede4e577c5baa8c4a8da2147970fb0853d7
Adepusrikanth9666 <64652479+Adepusrikanth9666@users.noreply.github.com>
2020-10-21 21:51:51 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Para from "./Para";
 
 class App extends Component {
     constructor(props) {
 		super(props);
-	};
+		this.state={
+			show:false
+		};
+	}
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				<button id="click" onClick={()=>this.setState({ show: !this.state.show })}></button>
+				{this.state.show?<Para />:null}
     		</div>
+			
     	);
     }
 }
